9b9a2f9df044883bd534b207a85a3ec21493be11
Christophe KEDE <ckede@athoria.fr>
2023-01-19 04:53:18 +0100

    add recusirve poste

--- liste_diffusion.php
+++ liste_diffusion.php
@@ -36,10 +36,6 @@ error_reporting(E_ALL);
                         require('inc/db.php');
                         //Object
                         $arborescence = array(
-                            'items' => array(),
-                            'parents' => array(),
-                            "poste" => array(),
-                            "contact" => array(),
                             "postes" => array(),
                         );
                         $query = "SELECT id,  nom FROM Liste ORDER BY nom asc";
@@ -49,17 +45,17 @@ error_reporting(E_ALL);
                         while ($listes = $queryGetAllListes->fetch(PDO::FETCH_ASSOC)) {
                             $arborescence['listes'][$listes['id']] = $listes;
                         }
-                        $query = 'SELECT ent0.Nom as entParent0, ent1.Nom as entParent1, pos.Nom as nom,
-                         pos.id as id, md.mode, plmd.listeID as listeID
-                        FROM poste_liste_mode_diffusion as plmd  join Poste as pos on plmd.posteID = pos.id  join
-                        mode_diffusion as md on (md.id = plmd.modeID) join Entite as ent0 on (pos.Entite=ent0.id)
-                        join Entite as ent1 on (ent0.Uper_id=ent1.id)';
+                        // $query = 'SELECT ent0.Nom as entParent0, ent1.Nom as entParent1, pos.Nom as nom,
+                        //  pos.id as id, md.mode, plmd.listeID as listeID
+                        // FROM poste_liste_mode_diffusion as plmd  join Poste as pos on plmd.posteID = pos.id  join
+                        // mode_diffusion as md on (md.id = plmd.modeID) join Entite as ent0 on (pos.Entite=ent0.id)
+                        // join Entite as ent1 on (ent0.Uper_id=ent1.id)';
+                        $query = "CALL liste_poste_recursive()";
                         $queryGetAllPostes = $db->prepare($query);
                         $queryGetAllPostes->execute();
                         while ($postes = $queryGetAllPostes->fetch(PDO::FETCH_ASSOC)) {
                             $arborescence['postes'][$postes['listeID']][] = $postes;
                         }
-
 
                         function getAllModeDiffusion()
                         {
@@ -109,7 +105,7 @@ error_reporting(E_ALL);
                                 $html .= "<ul class='listree-submenu-items'>" . createButtonDownload($listeID);
                                 if (isset($arborescence['postes'][$listeID])) {;
                                     foreach ($arborescence['postes'][$listeID] as $poste) {
-                                        $posteName =  '<span class="text-primary">' . $poste['nom'] . '</span>\\' . $poste['entParent0'] . '\\' . $poste['entParent1'];
+                                        $posteName =  '<span class="text-primary">' . $poste['nom'] . '</span>-' . $poste['entitename'];
                                         $mode = $poste['mode'];
                                         $posteID = $poste['id'];
 
