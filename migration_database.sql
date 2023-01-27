730009d6234220b89bdba06c57ab4d5caf914f47
Christophe KEDE <ckede@athoria.fr>
2023-01-27 17:00:41 +0100

    Update  migration file

--- migration_database.sql
+++ migration_database.sql
@@ -102,3 +102,51 @@ with recursive cte  as (
 select Poste.id as id, cte.Nom as entitename, Poste.Nom as Nom from cte
 join Poste on (Poste.Entite = cte.id) WHERE lvl >0 and Uper_id = 0$$
 DELIMITER ;
+
+-- Migration 27/01/2023
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `recursive_poste_with_id`(IN `id_` INT UNSIGNED)
+with recursive cte2  as (
+  select     id,
+             Nom,
+             Uper_id,
+    		1 lvl
+  from       Entite
+
+  union all
+  select     p.id,
+             concat(t.Nom,'\\' ,p.Nom),
+             t.Uper_id,
+    			lvl+1
+
+  from       cte2 p,  Entite t
+          WHERE p.Uper_id = t.id
+)
+select Poste.id as id, cte2.Nom as entitename, Poste.Nom as Nom from cte2
+join Poste on (Poste.Entite = cte2.id) WHERE lvl >0 and Uper_id = 0 and Poste.id =id_$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `liste_poste_recursive_with_listeid`(IN `listeID_` INT)
+with recursive cte(id, Nom, Uper_id, lvl)  as (
+  select     id,
+             Nom,
+             Uper_id,
+    		1 lvl
+  from       Entite
+
+  union all
+  select     p.id,
+             concat(t.Nom,'\\' ,p.Nom),
+             t.Uper_id,
+    			lvl+1
+
+  from       cte p,  Entite t
+          WHERE p.Uper_id = t.id
+)
+select pos.id as id, cte.Nom as entitename, pos.Nom as nom, md.mode, liste.Nom as listename, plmd.listeID as listeID from cte
+join Poste  as pos on (pos.Entite = cte.id)
+join poste_liste_mode_diffusion as plmd on ( plmd.posteID = pos.id)  join
+mode_diffusion as md on (md.id = plmd.modeID) join liste on (liste.id = plmd.listeID)
+WHERE lvl >0 and Uper_id = 0 and plmd.listeID = listeID_$$
+DELIMITER ;
